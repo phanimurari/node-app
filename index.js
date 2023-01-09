783712916fa3f52ec3bfab7f6fb620690a7bdb30
Phani Murari <imphanimurari1@gmail.com>
2023-01-09 16:39:42 +0000

    hbewj

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ app.get("/books/", async (request, response) => {
 // User Register API
 app.post("/users/", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
+
+  console.log(username, "username");
+  console.log(password, "password");
+  console.log(name, "name");
+  console.log(password, "pasowrd");
+  console.log(gender, "gender");
+  console.log(location, "location");
   const selectUserQuery = `
     SELECT 
       * 
